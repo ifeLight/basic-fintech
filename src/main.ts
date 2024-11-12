d1ca6b354497f0ef5f48fb145ea6f526bf2ba8c7
Ifedayo Karim <karimjnr@gmail.com>
2024-11-12 11:44:35 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Basic Fintech API')
+    .setDescription('The basic fintech API description')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+  const documentFactory = () => {
+    return SwaggerModule.createDocument(app, swaggerConfig);
+  };
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(3000);
 }
+
 bootstrap();
