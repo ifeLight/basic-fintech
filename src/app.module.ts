30ba227d5f2ae94a84e21e5e7a879a7d5be02657
Ifedayo Karim <karimjnr@gmail.com>
2024-11-12 08:04:17 +0100

    set up database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,10 +10,26 @@ import { WalletController } from './wallet/wallet.controller';
 import { UserService } from './user/user.service';
 import { WalletService } from './wallet/wallet.service';
 
+const ConfigModuleReUse = ConfigModule.forRoot({
+  load: [configuration],
+});
+
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
+    ConfigModuleReUse,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModuleReUse],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController, UserController, WalletController],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,13 @@
+// Purpose: Configuration file for the application.
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
     name: process.env.DATABASE_NAME || 'basic_fintech',
-    password: process.env.DATABASE_PASSWORD || 'basic_fintech',
+    password: process.env.DATABASE_PASSWORD || 'password',
     user: process.env.DATABASE_USER || 'root',
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true' || true,
+    logging: process.env.DATABASE_LOGGING === 'true' || false,
   },
 });
